6b1b15b278448885bb3fa608c7d867e18b12833a
Евгений <evgeniisergeev2002@list.ru>
2023-10-08 14:45:48 +0500

    Create FilmCard component

---
+++ src/components/FilmCard.tsx
@@ -0,0 +1,25 @@
+interface FilmCardProps {
+  name: string;
+  pageUrl: string;
+  imgUrl: string;
+}
+
+export function FilmCard(props: FilmCardProps) {
+  return (
+    <article className="small-film-card catalog__films-card">
+      <div className="small-film-card__image">
+        <img
+          src={props.imgUrl}
+          alt={props.name}
+          width={280}
+          height={175}
+        />
+      </div>
+      <h3 className="small-film-card__title">
+        <a className="small-film-card__link" href={props.pageUrl}>
+          {props.name}
+        </a>
+      </h3>
+    </article>
+  );
+}
