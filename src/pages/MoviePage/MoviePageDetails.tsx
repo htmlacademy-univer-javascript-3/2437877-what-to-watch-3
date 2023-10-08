b998e433536f619040347a8b7a575c892925582e
Евгений <evgeniisergeev2002@list.ru>
2023-10-08 19:03:55 +0500

    nano fix

--- src/pages/MoviePage/MoviePageDetails.tsx
+++ src/pages/MoviePage/MoviePageDetails.tsx
@@ -1,6 +1,7 @@
 import {MoviePageBase} from './MoviePageBase.tsx';
 import {FilmDetails} from '../../components/MoviePage/FilmDetails.tsx';
+import {Action} from '../../Elements/MyList.tsx';
 
 export const MoviePageDetails = () => (
-  <MoviePageBase content={<FilmDetails/>} activeTab='Details'/>
+  <MoviePageBase content={<FilmDetails/>} activeTab='Details' action={Action.Add}/>
 );
