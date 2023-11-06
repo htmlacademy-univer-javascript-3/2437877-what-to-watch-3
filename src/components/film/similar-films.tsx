dff1bd0beb16f9abc6315753a19dabc4b3079909
Евгений <11@11.11>
2023-11-06 13:41:35 +0500

    фикс названия пропсов

--- src/components/film/similar-films.tsx
+++ src/components/film/similar-films.tsx
@@ -1,11 +1,11 @@
 import {FilmList} from '@components/film/film-list.tsx';
 import {films} from '@mocks/films.ts';
 
-interface SimilarFilms {
+interface SimilarFilmsProps {
   filmGenre: string;
   excludeFilmId: number;
 }
 
-export const SimilarFilms = ({filmGenre,excludeFilmId}: SimilarFilms) => (
+export const SimilarFilms = ({filmGenre, excludeFilmId}: SimilarFilmsProps) => (
   <FilmList myFilms={films.filter((x) => x.genre === filmGenre && x.id !== excludeFilmId)}/>
 );
